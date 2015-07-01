f7e345873a722371f3c5873c8c91c7699586eba1
purvesta <purvesta0704@gmail.com>
2015-07-01 09:29:33 -0600

    jj
    jj

--- install/functions.php
+++ install/functions.php
@@ -14,13 +14,23 @@ function installForm(){
                     
                     <p align="center">Choose how to build your teacher emails!</p>
                     <div class="ui-grid-a">
-                    	<div id="templateButtonDiv" class="ui-block-a"><input type="button" value="Templates" name="templateButtonOption" id="templateButtonOption"  onClick="$('#templateButtonDiv').css('visibility', 'hidden');"/></div>
-                    	<div id="manualButtonDiv" class="ui-block-b"><input type="button" value="Manual Input" name="manualButtonOption" id="manualButtonOption" onClick="$('#manualButtonDiv').css('visibility', 'hidden');" /></div>                        
+                    	<div id="templateButtonDiv" class="ui-block-a"><input type="button" value="Templates" name="templateButtonOption" id="templateButtonOption"  onClick="$('#templateButtonDiv').css('visibility', 'visible');"/></div>
+                    	<div id="manualButtonDiv" class="ui-block-b"><input type="button" value="Manual Input" name="manualButtonOption" id="manualButtonOption" onClick="$('#manualTeacherEmailDiv').css('visibility', 'visible');" /></div>                        
                     </div>
-                    <!--
-                    <label for="manualTeacherEmailSetup" hidden="true">Enter teacher emails manually</label>
-                    <input type="text" name="manualTeacherEmailSetup" id="manualTeacherEmailSetup" placeholder="Teacher email goes here" hidden="true" />
-                    -->	
+                    
+                    <div id="manualTeacherEmailDiv" style="visibility: hidden;">
+                        <label for="manualTeacherEmailSetup">Enter teacher emails manually</label>
+                        <input type="text" name="manualTeacherEmailSetup" id="manualTeacherEmailSetup" placeholder="Teacher email goes here" hidden="true" />
+                    </div>
+                    
+                    <div>
+                    	<input type="radio" name="teacherEmailRadio" id="emailChoiceOne" />
+                        <input type="radio" name="teacherEmailRadio" id="emailChoiceTwo" />
+                        <input type="radio" name="teacherEmailRadio" id="emailChoiceThree" />
+                        <input type="radio" name="teacherEmailRadio" id="emailChoiceFour" />
+                        <input type="radio" name="teacherEmailRadio" id="emailChoiceFive" />
+                    </div>
+                    
                     <input type="submit" name="submit" value="Submit" />
                 </form>
             </div>
