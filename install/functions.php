f7f7f4e33f3eddcf1b9fca0104a306859a0551da
miascholes <miascholes@gmail.com>
2015-07-06 10:30:46 -0600

    domain stuff

--- install/functions.php
+++ install/functions.php
@@ -41,7 +41,7 @@ function installForm(){
                             <option value="emailChoice6">purvest</option>
                         </select>
 
-                        <label for="Jeffery">Type second half of email below: (i.e. hostname.domain would be purvesta@hostname.domain) </label>
+                        <label for="Jeffery">Type second half of email below: <br /> (i.e. typing "hostname.domain" would create the email "purvesta@hostname.domain") </label>
                         <input type="text" id="emailDomain" name="Jeffery" />
 
 
