1fd1a9c61049bc6afb126e7dfbf25431f1ea1a11
purvesta <purvesta0704@gmail.com>
2015-07-10 10:58:01 -0600

    Some changes

--- admin/settings/functions.php
+++ admin/settings/functions.php
@@ -116,13 +116,18 @@ function displaySettings(){
                             //Label is teacher name
 							$newVal = str_replace(' ', '', $value);
 							$nameVal = str_replace(',', '', $newVal);
+<<<<<<< Updated upstream
 							sort($value);
 							sort($nameVal);
                             echo '<fieldset class=\'ui-grid-a\'>
                                     <div class=\'ui-block-a\'>
+=======
+                            echo '<fieldset class="ui-grid-a">
+                                    <div class="ui-block-a" style="padding-top: 1.4%;">
+>>>>>>> Stashed changes
                                         <label >'.$value.'</label>
                                     </div>
-                                    <div class=\'ui-block-b\'>
+                                    <div class="ui-block-b">
                                         <input type=\'text\' data-clear-btn=\'true\' name=\''.$nameVal.'\' />
                                     </div>
                                   </fieldset>';
